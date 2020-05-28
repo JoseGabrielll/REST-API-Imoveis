50264333dca22c66805b4a9f77e4cfc390c63228
Gabriel <josegabrielpe@hotmail.com>
2020-05-28 14:43:14 -0300

    swagger adicionado

--- api/app.py
+++ api/app.py
@@ -3,8 +3,9 @@ from flask_restful import Api
 from resources.cliente import Clientes, Cliente
 from resources.imovel import Imoveis, Imovel
 from resources.contrato import Contratos, Contrato
+from flask_swagger_ui import get_swaggerui_blueprint
 
-#bp = Blueprint('app', __name__)
+bp = Blueprint('app', __name__)
 
 app = Flask(__name__)
 api = Api(app)
@@ -18,6 +19,17 @@ api.add_resource(Imovel, '/imoveis/<int:imovel_id>')
 api.add_resource(Contratos, '/contratos')
 api.add_resource(Contrato, '/contratos/<int:id_contrato>')
 
+SWAGGER_URL = '/docs'
+API_URL = '/static/swagger.json'
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  
+    API_URL,
+    config={  
+        'app_name': "Api Imoveis"
+    }
+)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 if __name__ == '__main__':
     app.run(debug=True)
